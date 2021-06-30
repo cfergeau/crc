2f913fcf51fcf0f6b7bc16967987c708765236f1
Christophe Fergeau <cfergeau@redhat.com>
2021-06-30 14:02:43 +0200

    non-working experiments
    Try to avoid 'go/bin/gotext extract   ./cmd/crc/cmd/' issues

--- cmd/crc/cmd/daemon_linux.go
+++ cmd/crc/cmd/daemon_linux.go
@@ -7,11 +7,13 @@ import (
 	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/logging"
 	"github.com/code-ready/gvisor-tap-vsock/pkg/transport"
+
+	"golang.org/x/text/message"
 )
 
 func vsockListener() (net.Listener, error) {
 	ln, err := transport.Listen(transport.DefaultURL)
-	logging.Infof("listening %s", transport.DefaultURL)
+	logging.Infof(message.Key("listening %s{DefaultURL}", "listening %s"), transport.DefaultURL)
 	if err != nil {
 		return nil, err
 	}
@@ -21,7 +23,7 @@ func vsockListener() (net.Listener, error) {
 func httpListener() (net.Listener, error) {
 	_ = os.Remove(constants.DaemonHTTPSocketPath)
 	ln, err := net.Listen("unix", constants.DaemonHTTPSocketPath)
-	logging.Infof("listening2 %s", constants.DaemonHTTPSocketPath)
+	logging.Infof(message.Key("listening %s{DaemonHTTPSocketPath}", "listening %s"), constants.DaemonHTTPSocketPath)
 	if err != nil {
 		return nil, err
 	}
