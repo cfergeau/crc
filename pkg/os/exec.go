7e880b4e6984f120ef9fe03aa91adfda94e625ce
Christophe Fergeau <cfergeau@redhat.com>
2021-09-06 17:44:19 +0200

    fixup! exec: Use ExecError returned from RunWithDefaultLocale/RunPrivileged

--- pkg/os/exec.go
+++ pkg/os/exec.go
@@ -56,6 +56,7 @@ func run(command string, args []string, env map[string]string) (string, string, 
 
 func runPrivate(command string, args []string, env map[string]string) (string, string, error) {
 	logging.Debugf("Running '%s <hidden arguments>'", command)
+	// todo: filter out ExecError
 	return runCmd(command, args, env)
 }
 
