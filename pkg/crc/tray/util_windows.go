a1d2d98743da122cdf1805b45a2c5c722d384128
Christophe Fergeau <cfergeau@redhat.com>
2021-09-21 18:31:42 +0200

    tray: Remove unused ValidateTrayAutostart

--- pkg/crc/tray/util_windows.go
+++
@@ -1,6 +0,0 @@
-package tray
-
-// ValidateTrayAutostart checks tray-auto-start is used in macOS and its a bool
-func ValidateTrayAutostart(value interface{}) (bool, string) {
-	return false, "Not supported on Windows"
-}
