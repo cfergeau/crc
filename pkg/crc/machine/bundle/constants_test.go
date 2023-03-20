db3ea958eeb2a4f3648ab5e2cdcab009ab6941c9
Christophe Fergeau <cfergeau@redhat.com>
2023-03-20 11:13:22 +0100

    bundle: Simplify constants_test.go
    As an alternative to the current test which first tests OpenShift URLs,
    and then MicroShift URLs, we can test all URLs in one go. Code is
    shorter, but the URI prefix is generated multiple times.

--- pkg/crc/machine/bundle/constants_test.go
+++ pkg/crc/machine/bundle/constants_test.go
@@ -13,22 +13,19 @@ import (
 )
 
 func TestBundleVersionURI(t *testing.T) {
-	checkBundleVersionURI(t, preset.OpenShift)
-	checkBundleVersionURI(t, preset.Microshift)
-}
 
-func checkBundleVersionURI(t *testing.T, p preset.Preset) {
-	bundleVersion := version.GetBundleVersion(p)
-	require.NotEqual(t, bundleVersion, "0.0.0-unset", fmt.Sprintf("%s version is unset (%s), build flags are incorrect", p, bundleVersion))
-
-	uriPrefix := fmt.Sprintf("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/%s/%s", p, bundleVersion)
 	for _, osInfo := range bundleLocations {
 		for _, presetInfo := range osInfo {
 			for preset, remoteFile := range presetInfo {
-				if preset == p {
-					assert.True(t, strings.HasPrefix(remoteFile.URI, uriPrefix), fmt.Sprintf("URI %s does not match %s version %s", remoteFile.URI, p, bundleVersion))
-				}
+				uriPrefix := getURIPrefix(t, preset)
+				assert.True(t, strings.HasPrefix(remoteFile.URI, uriPrefix), fmt.Sprintf("URI %s does not match %s version %s", remoteFile.URI, preset, version.GetBundleVersion(preset)))
 			}
 		}
 	}
+}
+
+func getURIPrefix(t *testing.T, preset preset.Preset) string {
+	bundleVersion := version.GetBundleVersion(preset)
+	require.NotEqual(t, bundleVersion, "0.0.0-unset", fmt.Sprintf("%s version is unset (%s), build flags are incorrect", preset, bundleVersion))
+	return fmt.Sprintf("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/%s/%s", preset, bundleVersion)
 }
